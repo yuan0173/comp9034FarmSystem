07e755f3da99bd47a3203862161cc111be9622a2
yuan0173 <yuan0173@flinders.edu.au>
2025-09-02 22:57:35 +0930

    update: change repository name

--- frontendWebsite/vite.config.ts
+++ frontendWebsite/vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/COMP9034-City--Farming-Industry-Time-Management-System/',
+  base: '/comp9034FarmSystem/',
   plugins: [
     react(),
     VitePWA({
@@ -35,18 +35,7 @@ export default defineConfig({
         theme_color: '#1976d2',
         background_color: '#ffffff',
         display: 'standalone',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
+        icons: [],
       },
     }),
   ],
